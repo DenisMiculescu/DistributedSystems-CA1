bcf88d840acbe60c240d270b6d438adb2773a0f2
denis miculescu <20098078@mail.wit.ie>
2024-11-07 14:33:06 +0000

    Lambda communicating with DynamoDB

--- lib/ca1-stack.ts
+++ lib/ca1-stack.ts
@@ -13,6 +13,7 @@ export class Ca1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //tables
     const teamsTable = new dynamodb.Table(this, "TeamsTable", {
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
@@ -20,22 +21,34 @@ export class Ca1Stack extends cdk.Stack {
       tableName: "Teams",
     });
 
-    const simpleFn = new lambdanode.NodejsFunction(this, "SimpleFn", {
-      architecture: lambda.Architecture.ARM_64,
-      runtime: lambda.Runtime.NODEJS_18_X,
-      entry: `${__dirname}/../lambdas/simple.ts`,
-      timeout: cdk.Duration.seconds(10),
-      memorySize: 128,
-    });
 
-    const simpleFnURL = simpleFn.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.AWS_IAM,  
+    //functions
+    const getTeamByIdFn = new lambdanode.NodejsFunction(
+      this,
+      "GetTeamByIdFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: `${__dirname}/../lambdas/getTeamById.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: teamsTable.tableName,
+          REGION: 'eu-west-1',
+        },
+      }
+    );
+
+    const getMovieByIdURL = getTeamByIdFn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
       cors: {
         allowedOrigins: ["*"],
       },
     });
 
-    new cdk.CfnOutput(this, "Simple Function Url", { value: simpleFnURL.url });
+    teamsTable.grantReadData(getTeamByIdFn)
+
+    new cdk.CfnOutput(this, "Get Movie Function Url", { value: getMovieByIdURL.url });
 
     new custom.AwsCustomResource(this, "teamsddbInitData", {
       onCreate: {
