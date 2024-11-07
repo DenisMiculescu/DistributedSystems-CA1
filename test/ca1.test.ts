84e57ab3ab2cc19519920b4102c8b0eefa18bf17
denis miculescu <20098078@mail.wit.ie>
2024-11-07 13:50:30 +0000

    Initial commit

---
+++ test/ca1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ca1 from '../lib/ca1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ca1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ca1.Ca1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
