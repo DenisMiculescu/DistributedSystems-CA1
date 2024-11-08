8f34d8d69b4cc524218f2a5c414d1b5e3690830e
denis miculescu <20098078@mail.wit.ie>
2024-11-08 15:15:16 +0000

    Setup for Auth API

--- lib/ca1-stack.ts
+++ lib/ca1-stack.ts
@@ -7,12 +7,24 @@ import { generateBatch } from "../shared/util";
 import { teams, teamMembers } from "../seed/teams";
 import * as apig from "aws-cdk-lib/aws-apigateway";
 import { Construct } from 'constructs';
+import { UserPool } from "aws-cdk-lib/aws-cognito";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as node from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class Ca1Stack extends cdk.Stack {
+
+  private auth: apig.IResource;
+  private userPoolId: string;
+  private userPoolClientId: string;
+  
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    //tables
+
+    //////////////////////////////////
+    //            tables            //
+    //////////////////////////////////
+
     const teamsTable = new dynamodb.Table(this, "TeamsTable", {
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
@@ -33,7 +45,11 @@ export class Ca1Stack extends cdk.Stack {
       sortKey: { name: "memberPosition", type: dynamodb.AttributeType.STRING },
     });
 
-    //functions
+
+    ////////////////////////////////////
+    //            functions           //
+    ////////////////////////////////////
+    
     const getTeamByIdFn = new lambdanode.NodejsFunction(
       this,
       "GetTeamByIdFn",
@@ -67,22 +83,6 @@ export class Ca1Stack extends cdk.Stack {
       }
     );
 
-    const getTeamMembersFn = new lambdanode.NodejsFunction(
-      this,
-      "GetTeamMember.Fn",
-      {
-        architecture: lambda.Architecture.ARM_64,
-        runtime: lambda.Runtime.NODEJS_16_X,
-        entry: `${__dirname}/../lambdas/getTeamMember.ts`,
-        timeout: cdk.Duration.seconds(10),
-        memorySize: 128,
-        environment: {
-          MEMBERS_TABLE_NAME: teamMembersTable.tableName,
-          REGION: "eu-west-1",
-        },
-      }
-    );
-
     const newTeamFn = new lambdanode.NodejsFunction(this, "AddTeamFn", {
       architecture: lambda.Architecture.ARM_64,
       runtime: lambda.Runtime.NODEJS_16_X,
@@ -106,6 +106,22 @@ export class Ca1Stack extends cdk.Stack {
         REGION: "eu-west-1",
       },
     });
+
+    const getTeamMembersFn = new lambdanode.NodejsFunction(
+      this,
+      "GetTeamMember.Fn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_16_X,
+        entry: `${__dirname}/../lambdas/getTeamMember.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          MEMBERS_TABLE_NAME: teamMembersTable.tableName,
+          REGION: "eu-west-1",
+        },
+      }
+    );
 
     new custom.AwsCustomResource(this, "teamsddbInitData", {
       onCreate: {
@@ -145,7 +161,11 @@ export class Ca1Stack extends cdk.Stack {
     //   },
     // });
 
-    // Permissions 
+
+    //////////////////////////////////////
+    //            Permissions           //
+    ////////////////////////////////////// 
+    
     teamsTable.grantReadData(getAllTeamsFn)
     teamsTable.grantReadData(getTeamByIdFn)
     teamMembersTable.grantReadData(getTeamByIdFn)
@@ -158,7 +178,10 @@ export class Ca1Stack extends cdk.Stack {
     // new cdk.CfnOutput(this, "Get Team Members Url", { value: getTeamMemberURL.url });
 
 
-    // REST API 
+    ////////////////////////////////
+    //          REST API          // 
+    ////////////////////////////////
+
     const CA1api = new apig.RestApi(this, "CA1 REST API", {
       description: "CA1 API",
       deployOptions: {
@@ -198,7 +221,66 @@ export class Ca1Stack extends cdk.Stack {
     teamMembersEndpoint.addMethod(
       "GET",
       new apig.LambdaIntegration(getTeamMembersFn, { proxy: true })
-);
+    );
+
+
+    //////////////////////////////////////////
+    //            Authentication            //
+    //////////////////////////////////////////
+
+    const userPool = new UserPool(this, "UserPool", {
+      signInAliases: { username: true, email: true },
+      selfSignUpEnabled: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    this.userPoolId = userPool.userPoolId;
+
+    const appClient = userPool.addClient("AppClient", {
+      authFlows: { userPassword: true },
+    });
+
+    this.userPoolClientId = appClient.userPoolClientId;
+
+    const authApi = new apig.RestApi(this, "AuthServiceApi", {
+      description: "Authentication Service RestApi",
+      endpointTypes: [apig.EndpointType.REGIONAL],
+      defaultCorsPreflightOptions: {
+        allowOrigins: apig.Cors.ALL_ORIGINS,
+      },
+    });
+
+    this.auth = authApi.root.addResource("auth");
 
   }
+  
+  private addAuthRoute(
+    resourceName: string,
+    method: string,
+    fnName: string,
+    fnEntry: string,
+    allowCognitoAccess?: boolean
+  ): void {
+    const commonFnProps = {
+      architecture: lambda.Architecture.ARM_64,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: "handler",
+      environment: {
+        USER_POOL_ID: this.userPoolId,
+        CLIENT_ID: this.userPoolClientId,
+        REGION: cdk.Aws.REGION
+      },
+    };
+    
+    const resource = this.auth.addResource(resourceName);
+    
+    const fn = new node.NodejsFunction(this, fnName, {
+      ...commonFnProps,
+      entry: `${__dirname}/../lambda/auth/${fnEntry}`,
+    });
+
+    resource.addMethod(method, new apig.LambdaIntegration(fn));
+  } 
 }
