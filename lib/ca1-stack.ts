8f6289a19ded6de4c6159603e0bbdae7d22eb47d
denis miculescu <20098078@mail.wit.ie>
2024-11-10 13:38:28 +0000

    Refactored stack into custom constructs

--- lib/ca1-stack.ts
+++ lib/ca1-stack.ts
@@ -10,6 +10,8 @@ import { Construct } from 'constructs';
 import { UserPool } from "aws-cdk-lib/aws-cognito";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as node from "aws-cdk-lib/aws-lambda-nodejs";
+import { AuthApi } from './auth-api'
+import {AppApi } from './app-api'
 
 export class Ca1Stack extends cdk.Stack {
 
@@ -184,6 +186,7 @@ export class Ca1Stack extends cdk.Stack {
 
     const CA1api = new apig.RestApi(this, "CA1 REST API", {
       description: "CA1 API",
+      endpointTypes: [apig.EndpointType.REGIONAL],
       deployOptions: {
         stageName: "dev",
       },
@@ -234,70 +237,22 @@ export class Ca1Stack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    this.userPoolId = userPool.userPoolId;
+    const userPoolId = userPool.userPoolId;
 
     const appClient = userPool.addClient("AppClient", {
       authFlows: { userPassword: true },
     });
 
-    this.userPoolClientId = appClient.userPoolClientId;
+    const userPoolClientId = appClient.userPoolClientId;
 
-    const authApi = new apig.RestApi(this, "AuthServiceApi", {
-      description: "Authentication Service RestApi",
-      endpointTypes: [apig.EndpointType.REGIONAL],
-      defaultCorsPreflightOptions: {
-        allowOrigins: apig.Cors.ALL_ORIGINS,
-      },
+    new AuthApi(this, 'AuthServiceApi', {
+      userPoolId: userPoolId,
+      userPoolClientId: userPoolClientId,
     });
 
-    this.auth = authApi.root.addResource("auth");
-
-    this.addAuthRoute(
-      "signup",
-      "POST",
-      "SignupFn",
-      'signup.ts'
-    );
-
-    this.addAuthRoute(
-      "confirm_signup",
-      "POST",
-      "ConfirmFn",
-      "confirm-signup.ts"
-    );
-
-    this.addAuthRoute('signout', 'GET', 'SignoutFn', 'signout.ts');
-    this.addAuthRoute('signin', 'POST', 'SigninFn', 'signin.ts');
-
-  }
-
-  private addAuthRoute(
-    resourceName: string,
-    method: string,
-    fnName: string,
-    fnEntry: string,
-    allowCognitoAccess?: boolean
-  ): void {
-    const commonFnProps = {
-      architecture: lambda.Architecture.ARM_64,
-      timeout: cdk.Duration.seconds(10),
-      memorySize: 128,
-      runtime: lambda.Runtime.NODEJS_18_X,
-      handler: "handler",
-      environment: {
-        USER_POOL_ID: this.userPoolId,
-        CLIENT_ID: this.userPoolClientId,
-        REGION: cdk.Aws.REGION
-      },
-    };
-    
-    const resource = this.auth.addResource(resourceName);
-    
-    const fn = new node.NodejsFunction(this, fnName, {
-      ...commonFnProps,
-      entry: `${__dirname}/../lambdas/auth/${fnEntry}`,
-    });
-
-    resource.addMethod(method, new apig.LambdaIntegration(fn));
+    new AppApi(this, 'AppApi', {
+      userPoolId: userPoolId,
+      userPoolClientId: userPoolClientId,
+    } );
   } 
 }
